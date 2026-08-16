aef1219c6b13a3a1b371e455e8a2b07905da59a8
lkerison <l.kerison@escolar.ifrn.edu.br>
2026-08-15 22:01:44 -0300

    Add menu and credits functionality to main.py
    Refactor main.py to include menu and credits options.

--- Snake/main.py
+++ Snake/main.py
@@ -1,4 +1,15 @@
 from telas.jogos import jogo
+from telas.menu import menu, creditos
+
 
 if __name__ == "__main__":
-    jogo()
+
+    while True:
+
+        tela_atual = menu()
+
+        if tela_atual == "jogo":
+            jogo()
+
+        elif tela_atual == "creditos":
+            creditos()
